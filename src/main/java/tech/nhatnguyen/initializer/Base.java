c4451e7c76a068ccb15e12ea8aa472c916e965bc
Nhat Nguyen <nhat@Akita-2.local>
2018-05-21 10:50:57 +0700

    Rename package, Add ExcelReader and EmailHelper

---
+++ src/main/java/tech/nhatnguyen/initializer/Base.java
@@ -0,0 +1,9 @@
+package tech.nhatnguyen.initializer;
+
+import org.openqa.selenium.WebDriver;
+
+public class Base {
+    public static WebDriver driver;
+
+
+}
---
+++ src/main/java/tech/nhatnguyen/initializer/StepHelper.java
@@ -0,0 +1,12 @@
+package tech.nhatnguyen.initializer;
+
+import tech.nhatnguyen.akitahelper.FunctionHelper;
+
+public class StepHelper {
+    public Base base;
+    public FunctionHelper functionHelper;
+
+    public StepHelper(Base base) {
+        this.base = base;
+    }
+}
